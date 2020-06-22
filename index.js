ec62369a8adb8626769fe2c3ac2d7d3116d1e20a
Jessica Trinh <jtjessicatrinh@gmail.com>
2020-06-22 14:44:14 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+/*  EXPRESS SETUP  */
+
+// create express app 
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname)); // directory to serve static files
+
+// Middleware
+const bodyParser = require('body-parser');
+const expressSession = require('express-session')({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(expressSession);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log('App listening on port ' + port));
